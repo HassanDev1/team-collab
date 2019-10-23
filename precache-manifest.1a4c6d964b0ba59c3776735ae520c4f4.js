814ed30410efbaa7bd68c78486631cc34a0847ec
Hassan <hassan.ali@g.austincc.edu>
2019-10-23 09:47:58 -0500

    Updates

---
+++ precache-manifest.1a4c6d964b0ba59c3776735ae520c4f4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e0a0d7dd052e8651fb8e281017f200d",
+    "url": "/team-collab/index.html"
+  },
+  {
+    "revision": "066ed92551ca2d9aa351",
+    "url": "/team-collab/static/css/2.5bc09813.chunk.css"
+  },
+  {
+    "revision": "1512a81f53341e9f132e",
+    "url": "/team-collab/static/css/main.349a9374.chunk.css"
+  },
+  {
+    "revision": "066ed92551ca2d9aa351",
+    "url": "/team-collab/static/js/2.7028168d.chunk.js"
+  },
+  {
+    "revision": "1512a81f53341e9f132e",
+    "url": "/team-collab/static/js/main.dd86e6eb.chunk.js"
+  },
+  {
+    "revision": "d5ed5370097a9854f803",
+    "url": "/team-collab/static/js/runtime-main.a62c244d.js"
+  },
+  {
+    "revision": "366949f801d4bc32780a9c704c9cb3a2",
+    "url": "/team-collab/static/media/collaborate.366949f8.png"
+  },
+  {
+    "revision": "e1e7fe4c80e5665e994f3499776f836a",
+    "url": "/team-collab/static/media/logo.e1e7fe4c.png"
+  },
+  {
+    "revision": "5ecbf2f9d6ac857d72d100b47b976d56",
+    "url": "/team-collab/static/media/track.5ecbf2f9.png"
+  }
+]);
